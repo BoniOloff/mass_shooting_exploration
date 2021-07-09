6267eaabe1da6c49c36e079f54bc29ce4b527e21
Boni Nugraha <bonioloff@gmail.com>
2021-07-09 15:55:37 -0500

    Prepare the map

--- app.R
+++ app.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(readr)
 library(lubridate)
+library(magrittr)
+library(leaflet)
 
 data <- read_csv("mass_shootings_1982_2021.csv")
 data$date <- mdy(data$date)
@@ -12,11 +14,19 @@ ui <- bootstrapPage(
     absolutePanel(top = 10, right = 10, id = 'controls',
                   sliderInput('nb_fatalities', 'Minimum Fatalities', 1, max(data$fatalities), 10),
                   dateRangeInput('date_range', 'Select Date Range', min(data$date), max(data$date)),
-    )
+    ),
+    tags$style(type = "text/css", "
+    html, body {width:100%;height:100%}
+    #controls{background-color:white;padding:20px;}
+    ")
 )
 
 server <- function(input, output) {
-    
+    output$map <- leaflet::renderLeaflet({
+        leaflet() %>%
+            addTiles() %>%
+            setView( -98.58, 39.82, zoom = 5)
+    })
 }
 
 shinyApp(ui = ui, server = server)
