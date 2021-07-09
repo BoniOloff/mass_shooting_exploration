99605cbff3bf55bec9fccd1d515910dc6a690d10
Boni Nugraha <bonioloff@gmail.com>
2021-07-09 11:23:52 -0500

    Prepare the UI design

--- app.R
+++ app.R
@@ -1,12 +1,18 @@
 library(shiny)
 library(readr)
+library(lubridate)
 
 data <- read_csv("mass_shootings_1982_2021.csv")
+data$date <- mdy(data$date)
 
-ui <- fluidPage(
-
-    titlePanel("Mass Shootings Exploration")
-    
+ui <- bootstrapPage(
+    title = "Mass Shooting Exploration",
+    theme = shinythemes::shinytheme('yeti'),
+    leaflet::leafletOutput('map', width = '100%', height = '100%'),
+    absolutePanel(top = 10, right = 10, id = 'controls',
+                  sliderInput('nb_fatalities', 'Minimum Fatalities', 1, max(data$fatalities), 10),
+                  dateRangeInput('date_range', 'Select Date Range', min(data$date), max(data$date)),
+    )
 )
 
 server <- function(input, output) {
